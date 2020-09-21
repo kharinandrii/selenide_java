67691a04d92e6dc6139f2388c8a75e379f7f4af7
AndyQa <andrej.kharin@gmail.com>
2020-09-21 16:21:09 +0100

    add configurations

--- src/test/java/us/abstracta/opencart/BaseTest.java
+++ src/test/java/us/abstracta/opencart/BaseTest.java
@@ -1,9 +1,14 @@
 package us.abstracta.opencart;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.WebDriver;
 
 public abstract class BaseTest {
     static {
         Configuration.baseUrl = "http://opencart.abstracta.us";
+        Configuration.browser = WebDriverRunner.FIREFOX;
+        Configuration.startMaximized = true;
+        //Configuration.headless = true;
     }
 }
