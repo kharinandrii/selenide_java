a9001737ac82801920e80ffaca2c2d464121ef53
AndyQa <andrej.kharin@gmail.com>
2020-09-21 13:55:59 +0100

    add MainPage and create 2 metods

--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -1,9 +1,24 @@
 package Pages;
 
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class MainPage {
+    private  SelenideElement myAccount = $(By.xpath("//*[text() = 'My Account']"));
+    private SelenideElement registerButton = $x("//*[text() = 'Register']");
+
+    public MainPage clickOnMyAccount() {
+    myAccount.click();
+    return this;
+}
+public MainPage clickOnRegisterButton() {
+    registerButton.click();
+    return this;
+}
 
 
 }
--- src/test/java/us/abstracta/opencart/RegisterTests.java
+++ src/test/java/us/abstracta/opencart/RegisterTests.java
@@ -2,21 +2,26 @@ package us.abstracta.opencart;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+import Pages.MainPage;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class RegisterTests {
     Params params = new Params();
+    MainPage mainPage = new MainPage();
     @BeforeEach
     void openSite() {
         open(params.URL);
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
     }
 
     @Test
     public void testRegisterUser() {
-        $x("//*[text() = 'My Account']").click();
-        $x("//*[text() = 'Register']").click();
-
+        mainPage.clickOnMyAccount()
+                .clickOnRegisterButton();
     }
 }
