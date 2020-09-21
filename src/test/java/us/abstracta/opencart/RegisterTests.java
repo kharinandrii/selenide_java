57169015a746b09af0f6c8919d0c65aba58fe752
AndyQa <andrej.kharin@gmail.com>
2020-09-21 12:00:07 +0100

    add @BeforeEach and

--- src/test/java/us/abstracta/opencart/RegisterTests.java
+++ src/test/java/us/abstracta/opencart/RegisterTests.java
@@ -2,17 +2,21 @@ package us.abstracta.opencart;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class RegisterTests {
     Params params = new Params();
+    @BeforeEach
+    void openSite() {
+        open(params.URL);
+    }
 
     @Test
     public void testRegisterUser() {
-        open(params.URL);
-        $(By.xpath("//*[text() = 'My Account']")).click();
-        $(By.xpath("//*[text() = 'Register']")).click();
+        $x("//*[text() = 'My Account']").click();
+        $x("//*[text() = 'Register']").click();
 
     }
 }
